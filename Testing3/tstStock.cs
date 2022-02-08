687f6179d63594084f4a41f411bad1ee6b6e89d9
Leigh Bird <P2605962@my365.dmu.ac.uk>
2022-02-08 12:50:04 +0000

    Adding testing for the stock class

---
+++ Testing3/tstStock.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock aStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
+
+       
