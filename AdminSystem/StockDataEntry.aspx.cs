69fa0b57e98f14d9428f1656423af5ca35017051
Leigh Bird <P2605962@my365.dmu.ac.uk>
2022-02-17 16:25:42 +0000

    completed week 21's work - some code in comments unsure on

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,71 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        //clsStock aStock = new clsStock();
+        //capture the data
+        //aStock.bookId = txtbookId.Text;
+        //store the data in the session object
+        //Session["aStock"] = aStock;
+        //navigate to the user page
+        //Response.Redirect("StockViewer.aspx");
+
+        //create a new instance of clsStock
+        //clsStock aStock = new clsStock();
+        //capture the data
+        //aStock.bookSearches = txtbookSearches.Text;
+        //store the data in the session object
+        //Session["aStock"] = aStock;
+        //navigate to the user page
+        //Response.Redirect("StockViewer.aspx");
+
+        //create a new instance of clsStock
+        clsStock aStock = new clsStock();
+        //capture the data
+        aStock.bookDescription = txtbookDescription.Text;
+        //store the data in the session object
+        Session["aStock"] = aStock;
+        //navigate to the user page
+        Response.Redirect("StockViewer.aspx");
+
+        //create a new instance of clsStock
+        //clsStock aStock = new clsStock();
+        //capture the data
+        //aStock.price = txtprice.Text;
+        //store the data in the session object
+        //Session["aStock"] = aStock;
+        //navigate to the user page
+        //Response.Redirect("StockViewer.aspx");
+
+        //create a new instance of clsStock
+        //clsStock aStock = new clsStock();
+        //capture the data
+        //aStock.dayAdded = txtdayAdded.Text;
+        //store the data in the session object
+        //Session["aStock"] = aStock;
+        //navigate to the user page
+        //Response.Redirect("StockViewer.aspx");
+
+        //create a new instance of clsStock
+        //clsStock aStock = new clsStock();
+        //capture the data
+        //aStock.available = chkavailable.Text;
+        //store the data in the session object
+        //Session["aStock"] = aStock;
+        //navigate to the user page
+        //Response.Redirect("StockViewer.aspx");
+
 
     }
 }
--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //create a new instance of clsStock
+        clsStock aStock = new clsStock();
+        //get data from the session object
+        aStock = (clsStock)Session["aStock"];
+        //display the data for this entry
+        Response.Write(aStock.bookId);
+        Response.Write(aStock.bookSearches);
+        Response.Write(aStock.bookDescription);
+        Response.Write(aStock.price);
+        Response.Write(aStock.dayAdded);
+        Response.Write(aStock.available);
 
     }
 }
